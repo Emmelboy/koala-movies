fe8fa59093876292308940f93e72afc3589edf6d
AloeKoala <aloekoala@gmail.com>
2026-05-11 21:47:49 +0300

    add dates

--- src/entities/movie/config/movies.data.ts
+++ src/entities/movie/config/movies.data.ts
@@ -414,4 +414,4 @@ export const movies: Movie[] = [
   { title: 'Я иду искать', year: 2019, rating: 6.9, chat: 7.0, date: '2024-04-05', nick: 'lord_cthulhu_23' },
   { title: 'Я потеряла свое тело', year: 2019, rating: 7.5, chat: 6.5, date: '2024-05-03', nick: 'zhrun1' },
   { title: 'Я хочу съесть твою поджелудочную', year: 2018, rating: 7.9, chat: 6.0, date: '2024-07-26', nick: 'zhrun1' },
-  { title: 'Я, Эрл и умирающая девушка', year: 2015, rating: 7.7, date: '2022-10-04', nick: 'daddypancake' },
+  { title: 'Я, Эрл и умирающая девушка', year: 2015, rating: 7.7, date: '2022-10-04', nick: 'daddypancake' },]
